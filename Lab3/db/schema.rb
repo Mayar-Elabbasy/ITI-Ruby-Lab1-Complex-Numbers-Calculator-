5561590c95d52e82ccb9c4199ab18bde58ed4943
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-04-26 21:25:09 +0200

    added username field to Sign in & Sign up in devise gem

--- Lab3/db/schema.rb
+++ Lab3/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_152835) do
+ActiveRecord::Schema.define(version: 2020_04_26_191513) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -36,8 +36,10 @@ ActiveRecord::Schema.define(version: 2020_04_26_152835) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "articles"
