4df830ef3d286ed8a031afc00a2ef67109c1a76f
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-04-27 14:17:40 +0200

    made migration so cancancan gem works correctly

--- Lab3/app/models/user.rb
+++ Lab3/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :articles
+  attr_accessor :username
+  validates_uniqueness_of :username
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- Lab3/db/schema.rb
+++ Lab3/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_191513) do
+ActiveRecord::Schema.define(version: 2020_04_26_210333) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -39,8 +41,8 @@ ActiveRecord::Schema.define(version: 2020_04_26_191513) do
     t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
 end
